3e7aff2e2bbe87f3642e16ecf56240820e914ada
cj970116 <1586556132@qq.com>
2020-03-20 00:30:37 +0800

    Initial Commit

---
+++ env/index.js
@@ -0,0 +1,14 @@
+/* 
+接口环境配置，方便接口服务器发生变化做出迁移
+*/
+module.exports = {
+  Dev:{
+    baseApi:'http://localhost:3000'
+  },
+  Test:{
+
+  },
+  Pro:{
+    
+  }
+}
---
+++ http/api.js
@@ -0,0 +1,7 @@
+/* 
+接口管理
+*/
+module.exports = {
+  getSession:'/api/mp/getSession',
+  login:'/api/mp/login'
+}
